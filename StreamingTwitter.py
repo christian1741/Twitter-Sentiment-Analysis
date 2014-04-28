ce57dc503a6fc196401946a868fffe1172f2a1a4
Christian <christian1741@users.noreply.github.com>
2014-04-28 16:26:27 -0500

    Update StreamingTwitter.py

--- StreamingTwitter.py
+++ StreamingTwitter.py
@@ -1,6 +1,13 @@
 #Christian Caicedo | April 2014
-#christiancaicedo.com/myblog
-#@caicedoc1741
-#Python file that streams tweets from the Twitter API 
+#christiancaicedo.com/myblog | Twitter: #@caicedoc1741
+#This file streams tweets from the Twitter API and saves them into a file 
 #It will create a file with thousands of positive and negative tweets  
 #The format is already set up, but you can modify it too.  
+
+import sys
+import tweepy
+import re 
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from nltk.corpus import stopwords
