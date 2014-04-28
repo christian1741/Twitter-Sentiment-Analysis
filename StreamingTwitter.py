b7ce2d10d6a0cb127793cf0ab4e8149d6036a3e7
Christian <christian1741@users.noreply.github.com>
2014-04-28 16:27:59 -0500

    Update StreamingTwitter.py

--- StreamingTwitter.py
+++ StreamingTwitter.py
@@ -9,5 +9,77 @@ import tweepy
 import re 
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
-from tweepy import Stream
-from nltk.corpus import stopwords
+
+#******Creating a dataset with POSITIVE Tweets*********** 
+
+class CustomStreamListener(tweepy.StreamListener):
+    def __init__(self, maxnum, api=None):
+        self.n = maxnum
+        self.api = api
+
+    def on_status(self, status):
+        try:
+            tweets = []
+            name = status.author.screen_name
+            textTwitter = status.text
+            
+            #Convert into lowercase
+            Tweet = textTwitter.lower()
+            
+            #Convert www.* or https?://* to URL
+            Tweet = re.sub('((www\.[\s]+)|(https?://[^\s]+))','URL',Tweet)
+            
+            #Convert @username to User
+            Tweet = re.sub('@[^\s]+','TWITTER_USER',Tweet)
+            
+            #Remove additional white spaces
+            tweet = re.sub('[\s]+', ' ', Tweet)
+            
+            #Replace #word with word Handling hashtags
+            Tweet = re.sub(r'#([^\s]+)', r'\1', Tweet)
+            
+            #trim
+            Tweet = Tweet.strip('\'"')
+            
+            #Deleting happy and sad face emoticon from the tweet 
+            a = ':)'
+            b = ':('
+            Tweet = Tweet.replace(a,'')
+            Tweet = Tweet.replace(b,'')
+            
+            #Deleting the Twitter @username tag and reTweets
+            tag = 'TWITTER_USER' 
+            rt = 'rt'
+            Tweet = Tweet.replace(tag,'')
+            Tweet = Tweet.replace(rt,'')
+            
+            final_tweet = '|positive|, |%s| ' % (Tweet)
+
+            f = open('YourFileHereName', 'r+')
+            old = f.read()
+            f.write(final_tweet+'\n')
+            f.close()
+        
+            self.n = self.n+1
+            if self.n < 250: 
+                return True
+            else:
+                print 'maxnum = '+str(self.n)
+                return False
+        
+        except Exception as e:
+            print (e)
+
+    def on_error(self, status_code):
+        #print >> sys.stderr, 'Encountered error with status code:', status_code
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        #print >> sys.stderr, 'Timeout...'
+        return True # Don't kill the stream
+        
+        
+        
+        
+        
+        
