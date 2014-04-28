f05ad3f5650ffc51f5c6d9eed5b3f0c885bc152a
Christian <christian1741@users.noreply.github.com>
2014-04-28 16:25:23 -0500

    Create StreamingTwitter.py

---
+++ StreamingTwitter.py
@@ -0,0 +1,6 @@
+#Christian Caicedo | April 2014
+#christiancaicedo.com/myblog
+#@caicedoc1741
+#Python file that streams tweets from the Twitter API 
+#It will create a file with thousands of positive and negative tweets  
+#The format is already set up, but you can modify it too.  
